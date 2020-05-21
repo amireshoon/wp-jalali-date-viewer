000c14d1373895bc8fb2d2e9b2dfe7af87756986
Amirhwsin Meidani <abtin.yas53@gmail.com>
2020-05-22 00:18:56 +0430

    placing files

---
+++ wp-jalali-date-show.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *
+ * @link              https://amirhwsin.ir
+ * @since             1.0.0
+ * @package           wp_jds
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Wordpress jalali date viewer
+ * Plugin URI:        https://amirhwsin.ir/wpjdv
+ * Description:       Used to show jalali date with shortcode.
+ * Version:           1.0.0
+ * Author:            Amirhossein Meydani
+ * Author URI:        https://amirhwsin.ir
+ * License:           MIT
+ * License URI:       
+ * Text Domain:       wp_jds
+ * Domain Path:       /languages
+ */
+
+ 
